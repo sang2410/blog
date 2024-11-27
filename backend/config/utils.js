044be0d6c973fd1a162569857d351d4ec55abaff
sang24102000 <chisang24102000@gmail.com>
2024-11-28 00:02:30 +0700

    edit file

--- backend/config/utils.js
+++ backend/config/utils.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const PORT = process.env.PORT;
-const MONGODB_URI = process.env.MONGODB_URI;
-const REDIS_URL = process.env.REDIS_URL;
+const MONGODB_URI ="mongodb://127.0.0.1/sang";
+const REDIS_URL = "redis://localhost:6379'";
 
 export { MONGODB_URI, PORT, REDIS_URL };
